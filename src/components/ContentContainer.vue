3faddcb44a17ebba495e8a355b04dcaa2bb27fec
Helton Mori <heltonmori@gmail.com>
2022-10-03 11:31:53 -0300

    Componentização da galeria

--- src/components/ContentContainer.vue
+++ src/components/ContentContainer.vue
@@ -1,26 +1,6 @@
 <template>
     <section class="main container row g-0">
-      <div class="col-6 gallery p-5">
-
-        <div class="product__gallery"
-          v-for="slide in slides" :key="slide">
-          <div class="">
-            <img class="product__image" :src="slide.src" alt="">
-          </div>
-        </div>
-          
-        <div class="thumbnails">          
-          <div class="thumb"
-            v-for="(thumb, index) in thumbs" :key="thumb">
-            <img 
-              class="img__thumb" 
-              :src="thumb.src" 
-              alt=""
-              @click="showImages(index)"
-            >
-          </div>
-        </div>
-      </div>
+      <ProductGallery></ProductGallery>
 
       <div class="col-6 description px-5">
 
@@ -57,87 +37,18 @@
 </template>
 
 <script>
-    export default {
-      data () {
-        return {
-          slides: [
-            { src: 'images/image-product-1.jpg' },
-            { src: 'images/image-product-2.jpg' },
-            { src: 'images/image-product-3.jpg' },
-            { src: 'images/image-product-4.jpg' }
-          ],
-          thumbs: [
-            { src: 'images/image-product-1-thumbnail.jpg'},
-            { src: 'images/image-product-2-thumbnail.jpg'},
-            { src: 'images/image-product-3-thumbnail.jpg'},
-            { src: 'images/image-product-4-thumbnail.jpg'}
-          ]
-        }
-      },
-      methods: {
-        showImages (n) {
-          let i
-          let imageIndex = n
-          let images = document.querySelectorAll(".product__image")
-          let thumbs = document.querySelectorAll(".img__thumb")
-          
-          for (i = 0; i < images.length; i++) {
-            images[i].style.display = "none"
-          }
-          for (i = 0; i < thumbs.length; i++) {
-            thumbs[i].className = thumbs[i].className.replace(" active", "")
-          }
-          images[imageIndex].style.display = "block"
-          thumbs[imageIndex].className += " active"
-        }
-      },
-      mounted () {
-        this.showImages(0)
-      }
+  import ProductGallery from './ProductGallery.vue';
+  export default {
+    data () {
+
+    },
+    components: {
+      ProductGallery
+    },
+    methods: {
+    },
+    mounted () {
+      
     }
+  }
 </script>
-
-<style scoped>
-.product__image {
-    display: none;
-    width: 100%;
-    border-radius: 16px;
-}
-
-.description {
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
-}
-
-.active {
-  opacity: 1;
-}
-
-.thumbnails {
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  margin-top: 32px;
-}
-
-.thumb {
-  width: 20%;
-  cursor: pointer;
-  transition: .3s all ease;
-}
-
-.thumb:hover {
-  opacity: .7;
-}
-
-.img__thumb {
-  width: 100%;
-  border-radius: 8px;
-}
-
-.img__thumb.active {
-  border: 3px solid #f2812c;
-  opacity: .7;
-}
-</style>
